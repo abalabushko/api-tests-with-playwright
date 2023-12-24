189d8460c32cd07edaf247b9c6ef7b75aafc17ec
Anna Balabushko <anna.balabushko92@gmail.com>
2023-12-24 10:11:20 +0100

    add tests for create user and login, remane file

---
+++ tests/api.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.parallel('Api testing', () => {
+  const baseURL = 'https://reqres.in/api';
+
+  test('Get user data', async ({ request }) => {
+    const response = await request.get(`${baseURL}/users/2`);
+    const responseBody = JSON.parse(await response.text());
+    
+    expect(response.status()).toBe(200);
+    expect(responseBody.data.id).toBe(2)
+ 
+    expect(responseBody.data.first_name).toBe('Janet');
+    expect(responseBody.data.last_name).toBe('Weaver');
+    expect(responseBody.data.avatar).toBeTruthy();
+  });
+
+  test('POST create a new user', async ({ request }) => {
+    const id = 500;
+    const response = await request.post(`${baseURL}/users`, {
+      data: {
+        id: id,
+      },
+    });
+    const responseBody = JSON.parse(await response.text());
+    
+    expect(response.status()).toBe(201);
+    expect(responseBody.id).toBe(id)
+    expect(responseBody.createdAt).toBeTruthy();
+  });
+
+
+  test('POST login', async ({ request }) => {
+
+    const response = await request.post(`${baseURL}/login`, {
+      data: {
+        email: "eve.holt@reqres.in",
+        password: "cityslicka"
+      },
+    });
+    const responseBody = JSON.parse(await response.text());
+    
+    expect(response.status()).toBe(200);
+    expect(responseBody.token).toBeTruthy();
+  });
+})
